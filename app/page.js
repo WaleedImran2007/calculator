e0ba45e82d558acdb7db496e9c78f9e70299c0e7
Waleed Imran <imrankhalida2009@gmail.com>
2025-08-10 14:12:57 +0500

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,103 +1,189 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React, { useEffect, useState, useRef } from "react";
+import './page.css';
+
+
+export default function CalculatorPage() {
+
+  const [display, setdisplay] = useState("0")
+
+  const handleClick = (value) => {
+    setdisplay((prev) =>
+      prev === "0" ? value.toString() : prev + value.toString()
+    )
+  }
+
+  const handleClear = () => {
+    setdisplay("0")
+  }
+
+  const handleEqual = () => {
+    try {
+
+      let expression = display;
+
+      const openCount = (expression.match(/\(/g) || []).length;
+      const closeCount = (expression.match(/\)/g) || []).length;
+      if (openCount > closeCount) {
+        expression += ")".repeat(openCount - closeCount);
+      }
+
+      expression = expression
+        .replace(/(\d+)%/g, "($1/100)")
+        .replace(/mod/g, "%")
+        .replace(/x/g, "*")
+        .replace(/÷/g, "/")
+        .replace(/pow/g, "**")
+        .replace(/sin\(([^)]+)\)/g, (_, angle) => {
+          const angleNum = eval(angle);
+          if (angleNum % 180 === 0) return "0";
+          return `Math.sin((${angle})*Math.PI/180)`;
+        })
+        .replace(/cos\(([^)]+)\)/g, (_, angle) => {
+          const angleNum = eval(angle);
+          if ((angleNum - 90) % 180 === 0) return "0";
+          return `Math.cos((${angle})*Math.PI/180)`;
+        })
+        .replace(/tan\(([^)]+)\)/g, (_, angle) => {
+          const angleNum = eval(angle);
+          if (angleNum % 90 === 0) {
+            if ((angleNum - 90) % 180 === 0) {
+              return angleNum % 180 === 90 ? "Infinity" : "-Infinity";
+            }
+            return "0";
+          }
+          return `Math.tan((${angle})*Math.PI/180)`;
+        })
+        .replace(/log\(([^)]+)\)?/g, (_, value) => `Math.log10(${value})`)
+        .replace(/ln\(([^)]+)\)?/g, (_, value) => `Math.log(${value})`);
+
+      let result = eval(expression);
+      setdisplay(result.toString());
+    } catch {
+      setdisplay("Error");
+    }
+  };
+
+  const deletePrev = () => {
+    setdisplay(display.slice(0, -1))
+  }
+
+  const handleSqrt = () => {
+    try {
+      setdisplay(eval(Math.sqrt(display)).toString())
+    }
+
+    catch {
+      setdisplay("Error")
+    }
+  }
+
+  const handleFactorial = () => {
+    let num = parseInt(display)
+    let fact = 1;
+
+    for (let index = 1; index <= num; index++) {
+      fact = fact * index;
+
+    }
+
+    setdisplay(fact)
+  }
+
+  const handleNegate = () => {
+    if (display === "0") return;
+    if (display.startsWith("-")) {
+      setdisplay(display.slice(1))
+    }
+    else {
+      setdisplay("-" + display)
+    }
+  }
+
+  const handleReciprocal = () => {
+    try {
+      const value = eval(display)
+      if (value === 0) {
+        setdisplay("Infinity");
+        return;
+      }
+      else {
+        const reciprocal = 1 / value;
+        setdisplay(reciprocal.toString())
+      }
+    }
+    catch {
+      setdisplay("Error")
+    }
+  }
+
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="min-h-screen text-white flex items-center justify-center bg-gradient-to-b from-gray-900 to-black p-6">
+      <div className="calculator flex flex-col w-full max-w-md sm:max-w-lg md:w-[60vw]">
+        <h2 className="text-2xl sm:text-3xl font-bold my-3 text-center sm:text-left">Calculator</h2>
+        <div className="bg-white/5 rounded-2xl p-4 shadow-2xl backdrop-blur-md min-h-[70vh]">
+          <input
+            onChange={(e) => setdisplay(e.target.value)}
+            value={display}
+            type="text"
+            className="text-right text-2xl sm:text-3xl bg-black w-full min-h-[10vh] rounded-2xl mx-auto outline-none p-3"
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="buttons grid grid-cols-5 gap-2 p-4">
+
+            {/* FIRST ROW */}
+            <button onClick={() => handleClick(Math.PI)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">π</button>
+            <button onClick={() => handleClick(Math.E)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">e</button>
+            <button onClick={() => handleClick("mod")} className="cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium flex justify-center shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">mod</button>
+            <button onClick={handleFactorial} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">!</button>
+            <button onClick={() => handleClear()} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-red-500 text-white hover:bg-red-700">C</button>
+
+            {/* SECOND ROW */}
+            <button onClick={() => handleClick("sin(")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">sin</button>
+            <button onClick={() => handleClick("cos(")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">cos</button>
+            <button onClick={() => handleClick("tan(")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">tan</button>
+            <button onClick={() => handleClick("log(")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">log</button>
+            <button onClick={() => handleClick("ln(")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">ln</button>
+
+            {/* THIRD ROW */}
+            <button onClick={() => { handleClick("(") }} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">(</button>
+            <button onClick={() => { handleClick(")") }} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">)</button>
+            <button onClick={() => handleClick("pow")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">x^y</button>
+            <button onClick={handleSqrt} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">√</button>
+            <button onClick={deletePrev} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">⌫</button>
+
+            {/* FOURTH ROW */}
+            <button onClick={() => handleClick(7)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">7</button>
+            <button onClick={() => handleClick(8)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">8</button>
+            <button onClick={() => handleClick(9)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">9</button>
+            <button onClick={() => handleClick("÷")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">÷</button>
+            <button onClick={handleReciprocal} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">1/x</button>
+
+            {/* FIFTH ROW */}
+            <button onClick={() => handleClick(4)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">4</button>
+            <button onClick={() => handleClick(5)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">5</button>
+            <button onClick={() => handleClick(6)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">6</button>
+            <button onClick={() => handleClick("x")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">x</button>
+            <button onClick={handleNegate} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">+/-</button>
+
+            {/* SIXTH ROW */}
+            <button onClick={() => handleClick(1)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">1</button>
+            <button onClick={() => handleClick(2)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">2</button>
+            <button onClick={() => handleClick(3)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">3</button>
+            <button onClick={() => handleClick("-")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">-</button>
+            <button onClick={() => handleClick("%")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">%</button>
+
+            {/* SEVENTH ROW */}
+            <button onClick={() => handleClick(0)} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10 col-span-2">0</button>
+            <button onClick={() => handleClick(".")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">.</button>
+            <button onClick={handleEqual} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-gradient-to-br from-indigo-500 to-purple-500 text-white">=</button>
+            <button onClick={() => handleClick("+")} className="flex justify-center cursor-pointer py-3 px-6 rounded-lg text-sm sm:text-base font-medium shadow-sm transition-transform active:scale-95 bg-white/6 text-white hover:bg-white/10">+</button>
+
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </main>
+  )
 }
